311900c9e65429f66884b4314e04168dfd5c4304
Zachary Allen <zachary.thomas.allen@outlook.com>
2020-02-19 08:15:49 -0500

    Update MenuLink.cs

--- Project/Project/MenuLink.cs
+++ Project/Project/MenuLink.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Project
 {
@@ -12,23 +12,44 @@ namespace Project
 		public void MenuLinks(int currentX, int currentY)
         {
 			
-			var Draw = new Draw();
-			if (currentX == 75 || 
-				currentX == 76 || 
-				currentX == 77 || 
-				currentX == 78 &&
-				currentY == 15 ||
-				currentY == 16 )
+			if (( currentX == 75 || currentX == 76 || currentX == 77 || currentX == 78 ) && ( currentY == 15 || currentY == 16 ))
             {
-				int ID = 0;
-				Console.Clear();
-				Program p = new Program();
-				p.GameEngine();
+				Engine(0);
 			}
-        }
+
+			if (( currentX == 27 || currentX == 28 || currentX == 29 || currentX == 30 ) && ( currentY == 9 || currentY == 10 ))
+			{
+				Engine(1);
+			}
+
+			if (( currentX == 100 || currentX == 101 || currentX == 102 || currentX == 103 ) && ( currentY == 19 || currentY == 20 ))
+			{
+				Engine(2);
+			}
+
+			if ((currentX == 25 || currentX == 26 || currentX == 27 || currentX == 28 ) && ( currentY == 23 || currentY == 24 ))
+			{
+				Engine(3);
+			}
+
+			if (( currentX == 65 || currentX == 66 || currentX == 67 || currentX == 68 ) && ( currentY == 37 || currentY == 38 ))
+			{
+				Engine(4);
+			}
+
+			if (( currentX == 130 || currentX == 131 || currentX == 132 || currentX == 133 ) && ( currentY == 5 || currentY == 6 ))
+			{
+				Engine(5);
+			}
+		}
 
 
-
+		public void Engine(int ID)
+		{
+			Console.Clear();
+			Program p = new Program();
+			p.GameEngine();
+		}
 	}
 
 }
